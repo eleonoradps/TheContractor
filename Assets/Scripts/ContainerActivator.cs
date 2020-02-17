a664f201a1c46d82cbf72c41a1458b35a02f6ed0
L <eleonora.dups@gmail.com>
2020-02-17 16:21:16 +0100

    added containers to each button & activate/deactivate

---
+++ Assets/Scripts/ContainerActivator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject sellerContainer;
+    [SerializeField] private GameObject storeContainer;
+    [SerializeField] private GameObject manufacturerContainer;
+
+    private bool isSellerContainerActive = false;
+    private bool isStoreContainerActive = false;
+    private bool isManufacturerContainerActive = false;
+
+    //isShapeContainerP1Active = !isShapeContainerP1Active;
+    //shapeContainerP1.SetActive(isShapeContainerP1Active);
+
+    public void ActivateSellerContainer()
+    {
+        isSellerContainerActive = !isSellerContainerActive;
+        sellerContainer.SetActive(isSellerContainerActive);
+    }
+
+    public void ActivateStoreContainer()
+    {
+        isStoreContainerActive = !isStoreContainerActive;
+        storeContainer.SetActive(isStoreContainerActive);
+    }
+    public void ActivateManufacturerContainer()
+    {
+        isManufacturerContainerActive = !isManufacturerContainerActive;
+        manufacturerContainer.SetActive(isManufacturerContainerActive);
+    }
+}
