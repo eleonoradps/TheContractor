fb55403b6553b451a3d92b9d4a9b6f08008e14aa
William PEPIN <williamp10022000@gmail.com>
2020-02-19 14:47:53 +0100

    added cooldown bar

---
+++ Assets/Scripts/CooldownBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownBar : MonoBehaviour
+{
+    Image cooldownBar;
+    [SerializeField] float maxTime;
+    float currentTime;
+    [SerializeField] float increasementPerSecond;
+    const float restartTime = 0;
+    
+    
+    void Start()
+    {
+        cooldownBar = GetComponent<Image>();
+        currentTime = restartTime;
+    }
+
+    
+    void Update()
+    {
+        if (currentTime < maxTime)
+        {
+            currentTime += Time.deltaTime * increasementPerSecond;
+            cooldownBar.fillAmount = currentTime / maxTime;
+        }
+
+        else
+        {
+            currentTime = restartTime;
+            this.transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
