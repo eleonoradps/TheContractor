9d3e3db60f986d0b91680a5e01b13097bed4f5f8
L <eleonora.dups@gmail.com>
2020-02-19 15:53:51 +0100

    added counter text

---
+++ Assets/Scripts/CounterManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterManager : MonoBehaviour
+{
+    [SerializeField] private int money;
+    [SerializeField] private TextMeshProUGUI textMoneyCounter;
+
+    [SerializeField] private int stock;
+    [SerializeField] private TextMeshProUGUI textStockCounter;
+
+    [SerializeField] private int merchandise;
+    [SerializeField] private TextMeshProUGUI textMerchCounter;
+    
+    public void AddMoney(int value)
+    {
+        money += value;
+        textMoneyCounter.text = money.ToString();
+    }
+
+    public void AddStock(int value)
+    {
+        stock += value;
+        textStockCounter.text = stock.ToString();
+    }
+
+    public void AddMerch(int value)
+    {
+        merchandise += value;
+        textMerchCounter.text = merchandise.ToString();
+    }
+}
