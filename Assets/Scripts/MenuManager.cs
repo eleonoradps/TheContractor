a1feb6b489afbea28769257c42054c0e3366e75d
L <eleonora.dups@gmail.com>
2020-02-19 15:18:48 +0100

    added game menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadPlayScene()
+    {
+        SceneManager.LoadScene("SceneLorna");
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
